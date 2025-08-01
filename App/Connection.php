2861da147ef0f4196cb197ebd240b519f9dbfd99
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-08-01 19:27:31 -0300

    Initial commit

---
+++ App/Connection.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use PDO;
+use PDOException;
+
+class Connection {
+    public static function getDb() {
+        try {
+            $conn = new PDO("mysql:host=localhost;dbname=mvc;charset=utf8", "php", "123456");
+            return $conn;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor/miniframework',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9fee9d88ef9014e8b792d5d113d52d9358e0315b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor/miniframework' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9fee9d88ef9014e8b792d5d113d52d9358e0315b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
