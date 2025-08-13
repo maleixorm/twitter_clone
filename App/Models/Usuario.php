49891791b7726a0f6ef127c02b258dab8f77475a
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-08-12 21:00:28 -0300

    Criando a model Usuário no projeto

---
+++ App/Models/Usuario.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use MF\Model\Model;
+
+class Usuario extends Model {
+    private $id;
+    private $nome;
+    private $email;
+    private $senha;
+
+    public function __get($atributo) {
+        return $this->$atributo;
+    }
+
+    public function __set($atributo, $valor) {
+        $this->$atributo = $valor;
+    }
+
+    public function salvar() {
+        $query = "INSERT INTO usuarios(nome, email, senha) VALUES (:nome, :email, :senha)";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':nome', $this->__get('nome'));
+        $stmt->bindValue(':email', $this->__get('email'));
+        $stmt->bindValue(':senha', $this->__get('senha'));
+        $stmt->execute();
+        return $this;
+    }
+}
