bb673cc8a22cb0697d713a90f7fade9cd85a4574
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-08-12 21:00:09 -0300

    Modificando o banco de dados a ser utilizado pela aplicação

--- App/Connection.php
+++ App/Connection.php
@@ -8,7 +8,7 @@ use PDOException;
 class Connection {
     public static function getDb() {
         try {
-            $conn = new PDO("mysql:host=localhost;dbname=mvc;charset=utf8", "php", "123456");
+            $conn = new PDO("mysql:host=localhost;dbname=twitter_clone;charset=utf8", "php", "123456");
             return $conn;
         } catch (PDOException $e) {
             echo $e->getMessage();
