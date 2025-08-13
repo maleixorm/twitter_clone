3839ca061e39fd8dc39b50b3c74169d78401de9c
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-08-12 22:59:43 -0300

    Adicionando validações ao registro dos usuários

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use App\Models\Usuario;
 use MF\Controller\Action;
 use MF\Model\Container;
 
@@ -11,13 +12,35 @@ class IndexController extends Action {
     public function index() {
         $this->render('index');
     }
+    
     public function inscreverse() {
+        $this->view->usuario = [
+            'nome' => '',
+            'email' => '',
+            'senha' => ''
+        ];
+        $this->view->erroCadastro = false;
         $this->render('inscreverse');
     }
+    
     public function registrar() {
-        echo '<pre>';
-        print_r($_POST);
-        echo '</pre>';
-        // $this->render('registrar');
+        $usuario = Container::getModel('Usuario');
+        $usuario->__set('nome', $_POST['nome']);
+        $usuario->__set('email', $_POST['email']);
+        $usuario->__set('senha', $_POST['senha']);
+
+        if ($usuario->validarCadastro() && count($usuario->getUsuarioPorEmail()) == 0) {
+            $usuario->salvar();
+            $this->render('cadastro');
+        } else {
+            $this->view->usuario = [
+                'nome' => $_POST['nome'],
+                'email' => $_POST['email'],
+                'senha' => $_POST['senha']
+            ];
+            $this->view->erroCadastro = true;
+            $this->render('inscreverse');
+        }
+        
     }
 }
