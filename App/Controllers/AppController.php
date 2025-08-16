90c909d9f5bb927df65b23e29587821ed72e8dbf
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-08-16 18:32:41 -0300

    Criando o controller AppController

---
+++ App/Controllers/AppController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\Usuario;
+use MF\Controller\Action;
+use MF\Model\Container;
+
+class AppController extends Action {
+    
+    public function timeline() {
+        session_start();
+
+        if (!empty($_SESSION['id']) && !empty($_SESSION['nome'])) {
+            $this->render('timeline');
+        } else {
+            header('Location: /?login=erro');
+        }
+    }
+}
