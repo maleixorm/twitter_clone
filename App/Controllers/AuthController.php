c72ea6878aec694d2bf0b4dc51d74c314cf077da
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-08-16 15:14:55 -0300

    Criando o controller de autenticação

---
+++ App/Controllers/AuthController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\Usuario;
+use MF\Controller\Action;
+use MF\Model\Container;
+
+class AuthController extends Action {
+    
+    public function autenticar() {
+
+    }
+}
