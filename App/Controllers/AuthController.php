04b98441d43fdb5d939520d5fe6e5ea87f88c758
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-08-16 15:52:21 -0300

    Criando a função autenticar para o controller de autenticação

--- App/Controllers/AuthController.php
+++ App/Controllers/AuthController.php
@@ -9,6 +9,12 @@ use MF\Model\Container;
 class AuthController extends Action {
     
     public function autenticar() {
-
+        $usuario = Container::getModel('Usuario');
+        $usuario->__set('email', $_POST['email']);
+        $usuario->__set('senha', $_POST['senha']);
+        $retorno = $usuario->autenticar();
+        echo '<pre>';
+        print_r($retorno);
+        echo '</pre>';
     }
 }
