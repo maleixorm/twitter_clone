043acca2d1ae4e6e1796e1a47871140618bc4a11
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-08-17 11:59:37 -0300

    Usando a criptografia para comparar a senha fornecida com a do banco de dados

--- App/Controllers/AuthController.php
+++ App/Controllers/AuthController.php
@@ -11,7 +11,7 @@ class AuthController extends Action {
     public function autenticar() {
         $usuario = Container::getModel('Usuario');
         $usuario->__set('email', $_POST['email']);
-        $usuario->__set('senha', $_POST['senha']);
+        $usuario->__set('senha', md5($_POST['senha']));
         $usuario->autenticar();
         
         if (!empty($usuario->__get('id')) && !empty($usuario->__get('nome'))) {
