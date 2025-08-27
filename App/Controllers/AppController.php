773a2cc9a49f491e725c95304d03371a23d3006f
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-08-27 16:27:39 -0300

    Refatorando código para criar uma função que valida autenticação

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -9,31 +9,27 @@ use MF\Model\Container;
 class AppController extends Action {
     
     public function timeline() {
-        session_start();
-
-        if (!empty($_SESSION['id']) && !empty($_SESSION['nome'])) {
-            $tweet = Container::getModel('Tweet');
-            $tweet->__set('id_usuario', $_SESSION['id']);
-            $tweets = $tweet->getAll();
-            $this->view->tweets = $tweets;
-            $this->render('timeline');
-        } else {
-            header('Location: /?login=erro');
-        }
+        $this->validaAutenticacao();
+        $tweet = Container::getModel('Tweet');
+        $tweet->__set('id_usuario', $_SESSION['id']);
+        $tweets = $tweet->getAll();
+        $this->view->tweets = $tweets;
+        $this->render('timeline');
+        
     }
 
     public function tweet() {
-        session_start();
+        $this->validaAutenticacao();
+        $tweet = Container::getModel('Tweet');
+        $tweet->__set('tweet', $_POST['tweet']);
+        $tweet->__set('id_usuario', $_SESSION['id']);
+        $tweet->salvar();
+    }
 
-        if (!empty($_SESSION['id']) && !empty($_SESSION['nome'])) {
-            $tweet = Container::getModel('Tweet');
-            $tweet->__set('tweet', $_POST['tweet']);
-            $tweet->__set('id_usuario', $_SESSION['id']);
-            $tweet->salvar();
-        } else {
+    public function validaAutenticacao() {
+        session_start();
+        if (!isset($_SESSION['id']) || empty($_SESSION['id']) || !isset($_SESSION['nome']) || empty($_SESSION['nome'])) {
             header('Location: /?login=erro');
         }
     }
-
-    
 }
