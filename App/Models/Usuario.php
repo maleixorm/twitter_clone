4631c53714c0bb3ac4600619434885bd51943920
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-09-07 20:29:24 -0300

    Implementando os métodos seguir e deixarSeguir do Usuario.

--- App/Models/Usuario.php
+++ App/Models/Usuario.php
@@ -81,10 +81,20 @@ class Usuario extends Model {
     }
 
     public function seguirUsuario($id_usuario_seguindo) {
-        echo 'seguir o usuario';
+        $query = "INSERT INTO usuarios_seguidores(id_usuario, id_usuario_seguindo) VALUES (:id_usuario, :id_usuario_seguindo)";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':id_usuario', $this->__get('id'));
+        $stmt->bindValue(':id_usuario_seguindo', $id_usuario_seguindo);
+        $stmt->execute();
+        return true;
     }
     
     public function deixarSeguirUsuario($id_usuario_seguindo) {
-        echo 'deixar de seguir o usuario';
+        $query = "DELETE FROM usuarios_seguidores WHERE id_usuario = :id_usuario AND id_usuario_seguindo = :id_usuario_seguindo";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':id_usuario', $this->__get('id'));
+        $stmt->bindValue(':id_usuario_seguindo', $id_usuario_seguindo);
+        $stmt->execute();
+        return true;
     }
 }
