a425defe0b8646b889c1540e215d59d960a65bbe
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-09-07 20:29:38 -0300

    Atualizando a consulta

--- querys/querys3.sql
+++ querys/querys3.sql
@@ -1,5 +1,9 @@
 create table usuarios_seguidores(
 	id int not null primary key auto_increment,
     id_usuario int not null,
-    id_usuario_seguidores int not null
+    id_usuario_seguindo int not null
 );
+
+ALTER TABLE usuarios_seguidores RENAME COLUMN id_usuario_seguidores TO id_usuario_seguindo;
+
+select * from usuarios_seguidores;
