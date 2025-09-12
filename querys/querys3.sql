b10ee74baeaa310b5add51822824320efb14548d
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-09-12 15:31:06 -0300

    Consulta a tabela tweets

--- querys/querys3.sql
+++ querys/querys3.sql
@@ -7,5 +7,5 @@ create table usuarios_seguidores(
 ALTER TABLE usuarios_seguidores RENAME COLUMN id_usuario_seguidores TO id_usuario_seguindo;
 
 select * from usuarios_seguidores;
-
+select * from tweets;
 select * from usuarios;
