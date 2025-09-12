f282c2a26c4ebc378dd43ca010ad916b007dfeb9
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-09-12 14:49:52 -0300

    Criando a rota para remover tweets

--- App/Route.php
+++ App/Route.php
@@ -61,6 +61,12 @@ class Route extends Bootstrap{
             'action' => 'acao'
         );
         
+        $routes['remover_tweet'] = array(
+            'route' => '/remover_tweet',
+            'controller' => 'AppController',
+            'action' => 'removerTweet'
+        );
+        
         $this->setRoutes($routes);
     }
 
